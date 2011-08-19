9c3cafa57def0c2f3aec8f1aba1373733b48552c
Steven Danna <steve@opscode.com>
2011-08-19 00:20:00 -0700

    Improve user_list

--- plugins/user_list.rb
+++ plugins/user_list.rb
@@ -2,10 +2,9 @@ module UserPlugin
   class UserList < Chef::Knife
     banner 'knife user list'
     def run
-      users = Array.new
       api_endpoint = "users"
       users = rest.get_rest(api_endpoint).map { |u| u["user"]["username"] }
-      ui.output(users)
+      ui.output(format_for_display(users.sort))
     end
   end
 end
