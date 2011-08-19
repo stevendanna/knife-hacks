7a5cda502a6d03397a8461afa2c87bd886c24575
Steven Danna <steve@opscode.com>
2011-08-19 00:08:09 -0700

    Add  to user_show plugin

--- plugins/user_show.rb
+++ plugins/user_show.rb
@@ -2,6 +2,14 @@ module UserPlugin
   class UserShow < Chef::Knife
     banner 'knife user show [USERNAME]'
 
+    attrs_to_show = []
+    option :attribute,
+    :short => "-a [ATTR]",
+    :long => "--attribute [ATTR]",
+    :proc => lambda {|val| attrs_to_show << val},
+    :description => "Show attribute ATTR"
+    
+
     def run
       if name_args.length < 1
         show_usage
@@ -11,7 +19,7 @@ module UserPlugin
       username = name_args[0]
       api_endpoint = "users/#{username}"
       user = rest.get_rest(api_endpoint)
-      ui.output(user)
+      ui.output(ui.format_for_display(user))
     end
   end
 end
