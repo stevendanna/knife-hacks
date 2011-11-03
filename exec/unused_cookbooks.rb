750f4535cbd5911e183d2d2aadf842b8cffb42c3
Steven Danna <steve@opscode.com>
2011-11-02 23:10:05 -0700

    Add unused_cookbooks script

---
+++ exec/unused_cookbooks.rb
@@ -0,0 +1,25 @@
+# unused_cookbooks.rb: List cookboosk that are note used by any nodes.
+#
+
+# First, we get a list of all the cookbooks
+all_cookbooks = api.get("cookbooks").keys
+
+used_cookbooks = Array.new
+
+# Next, we expand the run list of each node,
+# and use the API to determine cookbook
+# dependencies.
+nodes.all do |n|
+  expanded_run_list = n.expand!('server').recipes
+  cookbook_hash = api.post("environments/#{n.chef_environment}/cookbook_versions",
+                           {:run_list => expanded_run_list})
+  used_cookbooks += cookbook_hash.keys
+end
+
+# Remove duplicates
+used_cookbooks.uniq!
+
+# Diff the two lists
+puts "Unused Cookbooks:"
+puts all_cookbooks - used_cookbooks
+exit 0
